1d316c8801c1b443adce306d1c28d1cde5c97aa8
Alisher <zumanovaliser814@gmail.com>
2026-06-16 17:37:30 +0600

    Switch to createBrowserRouter, clean up index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import "./index.css";
-import { App } from ".";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello, world!</h1>,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );

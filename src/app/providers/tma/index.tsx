15ac1e1bf520869add8699ad00156af5731548fd
Alisher <zumanovaliser814@gmail.com>
2026-06-17 16:12:18 +0600

    feat: mount miniApp and viewport in TMAProvider; expand to fullscreen

--- src/app/providers/tma/index.tsx
+++ src/app/providers/tma/index.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode, useEffect } from "react";
 
-import { init, viewport } from "@tma.js/sdk-react";
+import { init, miniApp, viewport } from "@tma.js/sdk-react";
 
 try {
   init();
@@ -14,8 +14,17 @@ interface Props {
 
 export const TMAProvider = ({ children }: Props) => {
   useEffect(() => {
+    if (miniApp.mount.isAvailable()) {
+      miniApp.mount();
+      miniApp.setHeaderColor('#FFFFFF');
+      miniApp.setBgColor('#F4F5F7');
+      miniApp.ready();
+    }
+
     if (viewport.mount.isAvailable()) {
-      void viewport.mount();
+      void viewport.mount().then(() => {
+        viewport.expand();
+      });
     }
   }, []);
 

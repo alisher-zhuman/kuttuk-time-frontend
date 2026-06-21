4c4fa15a4f5ca11557c7b0d014573a84ffbca0eb
Alisher <zumanovaliser814@gmail.com>
2026-06-21 18:41:57 +0600

    style: sort exports in shared helpers index

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,4 +1,4 @@
 export { cn } from "./base";
 export { formatMoney } from "./money";
-export { getTMAUserInfo } from "./tma-user";
 export type { TmaUserInfo } from "./tma-user";
+export { getTMAUserInfo } from "./tma-user";

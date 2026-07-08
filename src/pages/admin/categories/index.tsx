d72311ce9d488775036dc159af6c5f8d38dbd23a
Alisher <zumanovaliser814@gmail.com>
2026-07-08 09:45:30 +0600

    style: remove bottom-clearance padding on category list

--- src/pages/admin/categories/index.tsx
+++ src/pages/admin/categories/index.tsx
@@ -5,7 +5,7 @@ import { Plus } from "lucide-react";
 import { CategoryList } from "@widgets/admin/categories";
 
 import { ROUTE_PATTERNS } from "@shared/constants";
-import { useNavigateTo, useSafeArea } from "@shared/hooks";
+import { useNavigateTo } from "@shared/hooks";
 import { Fab } from "@shared/ui";
 
 export const AdminCategoriesPage = () => {
@@ -13,14 +13,9 @@ export const AdminCategoriesPage = () => {
 
   const navigateTo = useNavigateTo();
 
-  const insets = useSafeArea();
-
   return (
     <>
-      {/* 72 = высота Fab (56) + отступ от края (16) */}
-      <div style={{ paddingBottom: insets.bottom + 72 }}>
-        <CategoryList />
-      </div>
+      <CategoryList />
 
       <Fab
         ariaLabel={t("admin.categories.create")}

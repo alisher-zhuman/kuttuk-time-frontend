9b0caa6b6ed265c90f336762d2e1e001266cc31b
Alisher <zumanovaliser814@gmail.com>
2026-07-08 16:21:53 +0600

    style: reduce category skeleton count to 11

--- src/widgets/admin/categories/ui/category-list/index.tsx
+++ src/widgets/admin/categories/ui/category-list/index.tsx
@@ -40,7 +40,7 @@ export const CategoryList = () => {
 
       {isLoading ? (
         <ul className="pb-5 flex flex-col gap-2 list-none">
-          {Array.from({ length: 13 }).map((_, i) => (
+          {Array.from({ length: 11 }).map((_, i) => (
             <li key={i}>
               <CategoryCardSkeleton />
             </li>
